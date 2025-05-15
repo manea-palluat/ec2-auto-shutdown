4da8f52795bdb0a7f790c1724b22c257c1b6aff5
Manea Palluat de Besset <manea.palluat@gmail.com>
2025-05-15 17:03:56 +0200

    refactor(lambda): cleaner code and enhanced shutdown notification message

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,36 +1,38 @@
-import os
-import boto3
+#This Lambda function stops EC2 instances and sends a notification via SNS.
 
-ec2 = boto3.client('ec2')
-sns = boto3.client('sns')
+import boto3, os #if you see an error here, don't worry, this code is not executed in this environment but in AWS Lambda which has boto3 installed
+
+ec2 = boto3.client('ec2') #this client allows us to interact with EC2 instances
+sns = boto3.client('sns') #and this one with SNS
+
+SNS_TOPIC_ARN = os.environ['SNS_TOPIC_ARN'] #this is the ARN of the SNS topic where we will send notifications
+INSTANCE_IDS  = os.environ['INSTANCE_IDS'].split(',') #this is a list of instance IDs that we want to stop, passed as a comma-separated string in the environment variable
+
+#this function is the entry point for the Lambda function
 
 def lambda_handler(event, context):
-    topic_arn = os.environ['SNS_TOPIC_ARN']
-    instance_ids = os.environ['INSTANCE_IDS'].split(',')
-
     stopped = []
-    already_stopped = []
-
-    statuses = ec2.describe_instance_status(InstanceIds=instance_ids, IncludeAllInstances=True)
-    for inst in statuses.get('InstanceStatuses', []):
-        state = inst['InstanceState']['Name']
-        iid = inst['InstanceId']
-        if state == 'running':
-            ec2.stop_instances(InstanceIds=[iid])
-            stopped.append(iid)
+    already = []
+    resp = ec2.describe_instance_status(InstanceIds=INSTANCE_IDS, IncludeAllInstances=True)
+    for inst in resp['InstanceStatuses']:
+        iid   = inst['InstanceId']
+        st    = inst['InstanceState']['Name']
+        if st == 'running': #if the instance is running...
+            ec2.stop_instances(InstanceIds=[iid]) #... we stop it
+            stopped.append(iid) #and we add it to the stopped list
         else:
-            already_stopped.append(iid)
+            already.append(iid) #... otherwise we add it to the already stopped list
 
-    message = ""
+    #and we send these information to the selected mail
+    parts = []
     if stopped:
-        message += f"Instances stopped: {', '.join(stopped)}.\n"
-    if already_stopped:
-        message += f"Instances already stopped: {', '.join(already_stopped)}."
-    if not message:
-        message = "No instances found."
+        parts.append(f"✅ Instances arrêtées : {', '.join(stopped)}") #cool pretty green check :)
+    if already:
+        parts.append(f"ℹ️ Instances déjà arrêtées : {', '.join(already)}") #info pretty blue info ;)
+    if not parts:
+        parts = ["ℹ️ Aucune instance trouvée."] #if no instance found, we send a message to the SNS topic
+    sns.publish(TopicArn=SNS_TOPIC_ARN, Subject="Alerte EC2 arrêt automatique", Message="\n".join(parts)) #email subject and attached message
 
-    sns.publish(
-        TopicArn=topic_arn,
-        Subject='EC2 Shutdown Report',
-        Message=message
-    )
+#Made by @manea-palluat (GitHub) - 2025
+#Hope you like it! If you have any question, feel free to ask me on GitHub or on my LinkedIn : https://www.linkedin.com/in/manea-palluat/
+#This code is provided as is, without any warranty. Use it at your own risk (Hoping you won't have any issue with it :P )
